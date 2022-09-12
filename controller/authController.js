2f8e911db9b301816dfcfa88425b339f9876328b
trevinkur <mokeyprojek@gmail.com>
2022-09-12 19:24:30 +0700

    adding status di controller

--- controller/authController.js
+++ controller/authController.js
@@ -6,7 +6,7 @@ module.exports = {
             const results = await Auth.login(req, res)
             return res.status(200).send(results)
         } catch (error) {
-            return res.status(500).send(error)
+            return res.status(error ? error.status ? error.status : 500 : 500).send(error)
         }
     },
     register: async (req, res)=> {
@@ -15,7 +15,7 @@ module.exports = {
             const results = await Auth.register(req, res)
             return res.status(200).send(results)
         } catch (error) {
-            return res.status(500).send(error)
+            return res.status(error ? error.status ? error.status : 500 : 500).send(error)
         }
     },
 }
